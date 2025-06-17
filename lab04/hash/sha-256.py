fe958e280650592ac863ccf2381cccbe9272f93d
MinhTuyetVoi <pata10102004@gmail.com>
2025-06-18 03:59:11 +0700

    Add lab04

---
+++ lab04/hash/sha-256.py
@@ -0,0 +1,18 @@
+import hashlib
+
+
+def calculate_sha256(data):
+    sha256_hash = hashlib.sha256()
+    sha256_hash.update(data.encode('utf-8'))
+    return sha256_hash.hexdigest()
+
+data_to_hash = input("Nhap du lieu can bam")
+hash_value = calculate_sha256(data_to_hash)
+print("Gia tri hash sha256:", hash_value)
+
+
+
+
+
+
+
